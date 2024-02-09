d79f2935690e41b0ec3bfb100c70a578709bb0c2
Facundo Altamira <facundo_altamira@outlook.com>
2024-02-08 21:32:07 -0300

    Agrego Oauth

--- api_biblioteca/routes/libros.js
+++ api_biblioteca/routes/libros.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const Libro = require("../models/Libro");
 
-router.get("/", async (req, res) => {
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
+
+router.get("/", requiredScopes("read:libros"), async (req, res) => {
   try {
     const libros = await Libro.find();
     res.json(libros);
@@ -11,15 +13,16 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Error al obtener los libros" });
   }
 });
-router.post("/", async (req, res) => {
+router.post("/", requiredScopes("write:libros"), async (req, res) => {
   try {
     const nuevoLibro = new Libro(req.body);
     await nuevoLibro.save();
+    res.json(nuevoLibro);
   } catch (error) {
     res.status(500).json({ error: "Error al crear el libro" });
   }
 });
-router.put("/:id", async (req, res) => {
+router.put("/:id", requiredScopes("write:libros"), async (req, res) => {
   try {
     const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -29,7 +32,7 @@ router.put("/:id", async (req, res) => {
     res.status(500).json({ error: "Error al actualizar el libro" });
   }
 });
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", requiredScopes("write:libros"), async (req, res) => {
   try {
     await Libro.findByIdAndDelete(req.params.id);
     res.json({ message: "Libro eliminado correctamente" });
